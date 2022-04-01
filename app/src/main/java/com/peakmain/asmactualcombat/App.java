62a594dc6798f587be059a1ce6e4089834150738
peakmain <2726449200@qq.com>
2022-04-01 14:29:34 +0800

    依赖版本升级

--- app/src/main/java/com/peakmain/asmactualcombat/App.java
+++ app/src/main/java/com/peakmain/asmactualcombat/App.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import com.peakmain.sdk.BuildConfig;
 import com.peakmain.sdk.SensorsDataAPI;
+import com.peakmain.sdk.constants.SensorsDataConstants;
 import com.peakmain.sdk.interfaces.OnUploadSensorsDataListener;
 import com.peakmain.sdk.utils.SensorsDataUtils;
 
@@ -20,8 +21,19 @@ public class App extends Application {
         SensorsDataAPI.init(this);
         SensorsDataAPI.getInstance().setOnUserAgreementListener(() -> true);
         SensorsDataAPI.getInstance().setOnUploadSensorsDataListener((state, data) -> {
-            if (BuildConfig.DEBUG) {
-                Log.e("TAG", data);
+            switch (state) {
+                case SensorsDataConstants.APP_START_EVENT_STATE:
+                case SensorsDataConstants.APP_END__EVENT_STATE:
+                case SensorsDataConstants.APP_VIEW_SCREEN__EVENT_STATE:
+                    if (BuildConfig.DEBUG) {
+                        Log.e("TAG", "埋点\n"+data);
+                    }
+                    break;
+                default:
+                    if (BuildConfig.DEBUG) {
+                        Log.e("TAG", data);
+                    }
+                    break;
             }
         });
     }
