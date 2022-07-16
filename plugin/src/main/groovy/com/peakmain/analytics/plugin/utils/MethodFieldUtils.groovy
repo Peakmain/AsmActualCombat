aaeee61d2280a344a1ae6f9efda4b179500ba465
peakmain <2726449200@qq.com>
2022-07-16 13:35:13 +0800

    插件更新1.1.2

--- plugin/src/main/groovy/com/peakmain/analytics/plugin/utils/MethodFieldUtils.groovy
+++ plugin/src/main/groovy/com/peakmain/analytics/plugin/utils/MethodFieldUtils.groovy
@@ -3,7 +3,7 @@ package com.peakmain.analytics.plugin.utils
 import org.objectweb.asm.Opcodes
 
 class MethodFieldUtils {
-    public static final String PLUGIN_VERSION = "1.1.1"
+    public static final String PLUGIN_VERSION = "1.1.2"
     enum StatusEnum {
         METHOD_STATE_NORMAL(0),
         METHOD_STATE_REPLACE(1)
